37e704999aa34caa758a8ab4300fb310fd0a9ce9
FukeKazki <kazkichi0906@gmail.com>
2022-07-04 22:09:55 +0900

    inital commit

---
+++ routes/_404.tsx
@@ -0,0 +1,17 @@
+// support jsx on deno deploy
+/** @jsxImportSource https://esm.sh/react@18.1.0 */
+
+import { Link } from "aleph/react";
+
+export default function E404() {
+  return (
+    <div className="screen e404">
+      <h2>
+        Ooooooops, nothing here!
+      </h2>
+      <p>
+        <Link to="/">Go back to the homepage</Link>
+      </p>
+    </div>
+  );
+}
